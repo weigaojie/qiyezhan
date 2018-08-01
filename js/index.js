7aee689c5541478a73f3d3c64b9ea1d79c6b755a
weigaojie <www.575684698@qq.com>
2018-08-01 23:32:32 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ window.onload=function(){
 	let mid=document.getElementsByClassName("mid")[0];
 	let top=mid.getElementsByClassName("top");
 	let yuan=document.getElementsByClassName("yuan");
+	let before=document.querySelector(".before");
+	let back=document.querySelector(".back");
 	for(let i=0;i<yuan.length;i++){
 		yuan[i].onclick=function(){
 			for(let j=0;j<yuan.length;j++){
@@ -12,6 +14,40 @@ window.onload=function(){
 			yuan[i].style.background="#000";
 		}
 	}
+	// top.onmouseenter=function(){
+	// 	before.style.display="block";
+	// 	back.style.display="block";
+	// }
+	// top.onmouseleave=function(){
+	// 	before.style.display="none";
+	// 	back.style.display="none";
+	// }
+	// let t=setInterval(move,2000);
+	// let width=parseInt(getComputedStyle(top,null).width);
+	// let now=0;
+	// let next=0;
+	// function move(){
+	// 	next++;
+	// 	if(next==top.length){
+	// 		next=0;
+	// 	}
+	// 	// for(let j=0;j<top.length;j++){
+	// 	// 	top[j].style.zIndex=5;
+	// 	// }
+	// 	top[next].style.left=width+"px";
+	// 	animate(top[now],{left:-width});
+ //        animate(top[next],{left:0});
+ //        now=next;
+	// }
+
+
+
+
+
+
+
+
+
 
 
 
